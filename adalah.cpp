3e2546a2ed0bf8bf698d6ef0992a7be438c98a87
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-03-09 10:57:48 +0700

    deklarasi x adalah bilangan bulat dan mengambil bilangan acak dari 0 sampe 9 yang akan dimasukkan ke variabel x

--- adalah.cpp
+++ adalah.cpp
@@ -4,13 +4,9 @@ using namespace std;
 int main()
 {
     int x;
-    
+
+    srand(time(0));
     x = rand() % 10;
 
-    if (x == 5)
-    {
-        x = x + 1;
-        cout << "Nilai x Sudah dikoreksi menjadi : " << x << endl;
-    }
-cout << "Bilangan yang diberikan = " << x;
+    
 }
