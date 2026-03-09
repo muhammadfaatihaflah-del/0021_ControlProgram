515e4f18c9d12cd1df93312a826b5a6f7f8b252a
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-03-09 10:53:38 +0700

    membuat program utama

--- adalah.cpp
+++ adalah.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    int x;
+    
+    x = rand() % 10;
+
+    if (x == 5)
+    {
+        x = x + 1;
+        cout << "Nilai x Sudah dikoreksi menjadi : " << x << endl;
+    }
+cout << "Bilangan yang diberikan = " << x;
+}
